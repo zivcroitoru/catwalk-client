c18b17b9aaff54bdc4c5f79a5de80a6c7b6d5f5e
zivcroitoru <zuverc@gmail.com>
2025-07-08 13:39:35 +0300

    connect client to server

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Catwalk client is running.");
+fetch('http://localhost:3000/api/test')
+  .then(res => res.json())
+  .then(data => {
+    console.log('Response from server:', data);
+    document.body.innerHTML += `<p>${data.message}</p>`;
+  })
+  .catch(err => {
+    console.error('Fetch error:', err);
+    document.body.innerHTML += `<p style="color:red;">Could not connect to server</p>`;
+  });
