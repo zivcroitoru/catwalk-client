37a2ced7c38c3e2519d3a2db4b6a003d7c801ac7
may861 <maysis6@gmail.com>
2025-07-29 01:15:06 +0300

    merge

--- js/core/auth/auth.js
+++ js/core/auth/auth.js
@@ -8,8 +8,6 @@ export function signOut() {
 
 export async function fetchUser() {
   try {
-    // const res = await fetch('https://catwalk-server.onrender.com/api/user', { credentials: 'include' });
-    // const data = await res.json();
     const username = localStorage.getItem("username");
     document.getElementById("welcomeMessage").textContent = `Welcome, ${username || 'Guest'}`;
   } catch {
