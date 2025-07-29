fdfeeac1b1f9554d8106b1fb812d3aed31745db9
zivcroitoru <zuverc@gmail.com>
2025-07-29 21:39:49 +0300

    add new cat temples json

--- js/core/init/dataLoader.js
+++ js/core/init/dataLoader.js
@@ -2,22 +2,32 @@
 
 export let userCats = [];
 export let shopItems = [];
-
 export async function loadAllData() {
   try {
-    const [catsRes, shopRes, breedsRes] = await Promise.all([
+    const [catsRes, shopRes, templatesRes] = await Promise.all([
       fetch("../data/usercats.json"),
       fetch("../data/shopItems.json"),
-      fetch("../data/breeds.json")
+      fetch("../data/cat_templates.json")
     ]);
 
     userCats = await catsRes.json();
     shopItems = await shopRes.json();
-    const breeds = await breedsRes.json();
+    const templates = await templatesRes.json(); // templates is an object, not an array
+
+    const breedItems = {};
+    for (const breed in templates) {
+      const cats = templates[breed];
+      breedItems[breed] = cats.map(cat => ({
+        name: cat.name,
+        variant: cat.variant,
+        palette: cat.palette,
+        sprite: cat.sprite
+      }));
+    }
 
     window.userCats = userCats;
     window.shopItems = shopItems;
-    window.breedItems = breeds;
+    window.breedItems = breedItems;
 
     console.log("✅ All data loaded");
   } catch (err) {
