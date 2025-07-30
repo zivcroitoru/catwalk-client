f06da7fd3ab8bf838b0399d179b176d4dce0bc60
may861 <maysis6@gmail.com>
2025-07-30 21:06:08 +0300

    checking run

--- cat-walk-admin/js-admin/login.js
+++ cat-walk-admin/js-admin/login.js
@@ -1,8 +1,6 @@
 // import { APP_URL } from "../../js/main.js";
 // Determine the backend URL based on the environment
-const APP_URL = window.location.hostname === 'localhost'
-  ? 'http://localhost:3000'
-  : 'https://catwalk-server.onrender.com';
+const APP_URL = window.location.hostname === 'https://catwalk-server.onrender.com';
  
 // import { APP_URL } from '../../js/main.js';
 console.log('APP_URL:', APP_URL);
