56fd9b587fba2136714d92d6e9d5db60b79e8748
zivcroitoru <zuverc@gmail.com>
2025-07-31 17:37:37 +0300

    lama ani hai

--- js/features/ui/carousel.js
+++ js/features/ui/carousel.js
@@ -46,7 +46,7 @@ export async function renderCarousel() {
   window.userCats = window.userCats.map(cat => ({
     ...cat,
     equipment: cat.equipment || { hat: null, top: null, eyes: null, accessories: [] },
-    image: cat.sprite
+    image: cat.sprite_url
   }));
 
   // Create cat cards
